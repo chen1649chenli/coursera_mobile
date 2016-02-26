9268666abc01ef4dc1047957ba1cc7c739b79aed
li chen <lichen@lidemacbook-air.gateway.pace.com>
2016-02-25 22:53:06 -0600

    week4-1 splash screen

--- conFusion/www/js/app.js
+++ conFusion/www/js/app.js
@@ -4,8 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.services'])
-.run(function($ionicPlatform, $rootScope, $ionicLoading) {
+angular.module('conFusion', ['ionic', 'ngCordova','conFusion.controllers','conFusion.services'])
+.run(function($ionicPlatform, $rootScope, $ionicLoading, $cordovaSplashscreen, $timeout) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -38,6 +38,10 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.service
       console.log('done');
       $rootScope.$broadcast('loading:hide');
   });
+
+  $timeout(function(){
+      $cordovaSplashscreen.hide();
+  },20000);
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
